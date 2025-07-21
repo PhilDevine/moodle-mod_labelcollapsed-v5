07f8b9546e0255e15e95f30826ccf2bc8b232c6b
PhilDevine <44176644+PhilDevine@users.noreply.github.com>
2025-07-21 16:45:34 +0100

    Update version.php
    new version details

--- version.php
+++ version.php
@@ -1,9 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = 'v5.0';
-$plugin->maturity = MATURITY_BETA;
-$plugin->version = 2025061805;
-$plugin->requires = 2022041900;
-$plugin->cron     = 0;
+$plugin->release   = 'v5.0-r2'; // Optional: increment release label
+$plugin->maturity  = MATURITY_BETA;
+$plugin->version   = 2025072100; // Updated to July 21, 2025
+$plugin->requires  = 2022041900;
+$plugin->cron      = 0;
 $plugin->component = 'mod_labelcollapsed';
